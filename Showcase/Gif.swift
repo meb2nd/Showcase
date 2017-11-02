000b895b190c4c618c6ff3dea065362f0a9e33d9
Pete Barnes <pete_barnes@me.com>
2017-11-01 22:49:39 -0400

    Remove artifact from file that was deleted.

---
+++ Showcase/Gif.swift
@@ -0,0 +1,34 @@
+//
+//  PlayerView.swift
+//  Showcase
+//
+//  Created by Pete Barnes on 10/30/17.
+//  Copyright © 2017 Pete Barnes. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayerView: UIView {
+    
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    override public class var layerClass:Swift.AnyClass {
+        get {
+            return AVPlayerLayer.self
+        }
+    }
+    
+}
